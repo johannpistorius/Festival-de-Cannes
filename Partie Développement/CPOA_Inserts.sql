bd3e854ad7079791e92e2ee714c0638167c30400
Alexis Perrot <alexis.perrot@etu.univ-lyon1.fr>
2017-01-04 15:56:26 +0100

    Premier commit, insertion partie analyse et initialisation partie développement.

---
+++ Partie Développement/CPOA_Inserts.sql
@@ -0,0 +1,131 @@
+INSERT INTO CPOA_Competition VALUES (1,'Longs Métrages');
+INSERT INTO CPOA_Competition VALUES (2,'Un Certain Regard');
+INSERT INTO CPOA_Competition VALUES (3,'Cinéfondation');
+INSERT INTO CPOA_Competition VALUES (4,'Courts Métrages');
+INSERT INTO CPOA_Competition VALUES (5,'Hors Compétition');
+
+INSERT INTO CPOA_Film VALUES (0,'CAROL',95,'Todd HAYNES',1);
+INSERT INTO CPOA_Film VALUES (1,'CHRONIC',126,'Michel FRANCO',1);
+INSERT INTO CPOA_Film VALUES (2,'DHEEPAN',102,'Jacques AUDIARD',1);
+INSERT INTO CPOA_Film VALUES (3,'IL RACCONTO DEI RACCONTI',96,'Matteo GARRONE',1);
+INSERT INTO CPOA_Film VALUES (4,'LA LOI DU MARCHE',103,'Stephane BRIZE',1);
+
+INSERT INTO CPOA_Film VALUES (5,'ALIAS MARIA',87,'José Luis RUGELES GRACIA',2);
+INSERT INTO CPOA_Film VALUES (6,'AN',99,'Naomi KAWASE',2);
+INSERT INTO CPOA_Film VALUES (7,'CHAUTHI KOOT',91,'Gurvinder SINGH',2);
+INSERT INTO CPOA_Film VALUES (8,'COMOARA',108,'Corneliu PORUMBOIU',2);
+INSERT INTO CPOA_Film VALUES (9,'HRÚTAR',92,'Grímur HÁKONARSON',2);
+
+INSERT INTO CPOA_Film VALUES (10,'14 STEPS',97,'Maksim SHAVKIN',3);
+INSERT INTO CPOA_Film VALUES (11,'ABWESEND',96,'Eliza PETKOVA',3);
+INSERT INTO CPOA_Film VALUES (12,'AINAHAN NE PALAA',92,'Salla SORRI',3);
+INSERT INTO CPOA_Film VALUES (13,'ANFIBIO',101,'Héctor SILVA NÚÑEZ',3);
+INSERT INTO CPOA_Film VALUES (14,'ASARA REHOVOT MEA ETSIM',91,'Miki POLONSKI',3);
+
+INSERT INTO CPOA_Film VALUES (15,'AVE MARIA',27,'Basil KHALIL',4);
+INSERT INTO CPOA_Film VALUES (16,'LE REPAS DOMINICAL',18,'Céline DEVAUX',4);
+INSERT INTO CPOA_Film VALUES (17,'LOVE IS BLIND',21,'Dan HODGSON',4);
+INSERT INTO CPOA_Film VALUES (18,'PATRIOT',16,'Eva RILEY',4);
+INSERT INTO CPOA_Film VALUES (19,'PRESENTE IMPERFECTO',23,'Iair SAID',4);
+
+INSERT INTO CPOA_Film VALUES (20,'INSIDE OUT',111,'Pete DOCTER',5);
+INSERT INTO CPOA_Film VALUES (21,'IRRATIONAL MAN',101,'Woody ALLEN',5);
+INSERT INTO CPOA_Film VALUES (22,'LA GLACE ET LE CIEL',92,'Luc JACQUET',5);
+INSERT INTO CPOA_Film VALUES (23,'LA TETE HAUTE',96,'Emmanuelle BERCOT',5);
+INSERT INTO CPOA_Film VALUES (24,'MAD MAX: FURY ROAD',118,'George MILLER',5);
+
+
+INSERT INTO CPOA_Personne VALUES (02,'COEN','Joel','06234567891','32 rue des fleurs','joel.coen@mail.com','1111111111');
+INSERT INTO CPOA_Personne VALUES (03,'COEN','Ethan','06345678912','33 rue des fleurs','ethan.coen@mail.com','2222222222');
+INSERT INTO CPOA_Personne VALUES (04,'DE PALMA','Rossy','0634567890','34 rue des fleurs','rossy.depalma@mail.com','3333333333');
+INSERT INTO CPOA_Personne VALUES (05,'DEL TORO','Guillermo','06456789123','35 rue des fleurs','guillermo.deltoro@mail.com','4444444444');
+
+INSERT INTO CPOA_Personne VALUES (06,'SISSAKO','Abderrahmane ','06567891234','36 rue des fleurs','abderrahmane.sissako@mail.com','5555555555');
+INSERT INTO CPOA_Personne VALUES (07,'DE FRANCE','Cécile','06678912345','37 rue des fleurs','cecile.defrance@mail.com','6666666666');
+INSERT INTO CPOA_Personne VALUES (08,'HADJITHOMAS','Joana','06789123456','38 rue des fleurs','joana.hadjithomas@mail.com','7777777777');
+INSERT INTO CPOA_Personne VALUES (09,'OLBRYCHSKI','Daniel','06891234567','39 rue des fleurs','daniel.olbrychski@mail.com','8888888888');
+INSERT INTO CPOA_Personne VALUES (10,'ZLOROWSSKI','Rebecca','06891234567','40 rue des fleurs','rebecca.zlorowski@mail.com','9999999999');
+
+INSERT INTO CPOA_Personne VALUES (11,'ROSSELLINI','Isabella','06912345678','41 rue des fleurs','isabella.rossellini@mail.com','0101010101');
+INSERT INTO CPOA_Personne VALUES (12,'Al-MANSOUR','Haifaa','06987654321','42 rue des fleurs','haifaa.almansour@mail.com','0202020202');
+INSERT INTO CPOA_Personne VALUES (13,'LABAKI','Nadine','06876543219','43 rue des fleurs','nadine.labaki@mail.com','0303030303');
+INSERT INTO CPOA_Personne VALUES (14,'Koutras','Panos','06765432198','44 rue des fleurs','panos.koutras@mail.com','0404040404');
+INSERT INTO CPOA_Personne VALUES (15,'RAHIM','Tahar','06654321987','45 rue des fleurs','tahar.rahim@mail.com','0505050505');
+
+INSERT INTO CPOA_Personne VALUES (16,'AZÉMA','Sabine','06543219876','46 rue des fleurs','sabine.azema@mail.com','0606060606');
+INSERT INTO CPOA_Personne VALUES (17,'GLEIZE','Delphine','06432198765','47 rue des fleurs','delphine.gleize@mail.com','0707070707');
+INSERT INTO CPOA_Personne VALUES (18,'POUPAUD','Melvil','06321987654','48 rue des fleurs','melvil.poupaud@mail.com','0808080808');
+INSERT INTO CPOA_Personne VALUES (19,'GARNIER','Claude','06219876543','49 rue des fleurs','claude.garnier@mail.com','0909090909');
+INSERT INTO CPOA_Personne VALUES (20,'PAYEN','Bernard','06198765432','50 rue des fleurs','bernard.payen@mail.com','0000000000');
+
+INSERT INTO CPOA_Salle VALUES(0,'Grand Théâtre Lumière',2400);
+INSERT INTO CPOA_Salle VALUES(1,'Salle Debusy',1000);
+INSERT INTO CPOA_Salle VALUES(2,'Salle Buñuel',500);
+INSERT INTO CPOA_Salle VALUES(3,'Salle Soixantième',1000);
+INSERT INTO CPOA_Salle VALUES(4,'Salle du Soixantième',1000);
+INSERT INTO CPOA_Salle VALUES(4,'Salle Bazin',500);
+
+INSERT INTO CPOA_VIP VALUES(01,'Française','Acteur','1966-08-06');
+INSERT INTO CPOA_VIP VALUES(04,'Espagnole','Réalisateur','1977-21-18');
+INSERT INTO CPOA_VIP VALUES(08,'Allemande','Réalisateur','1962-02-02');
+INSERT INTO CPOA_VIP VALUES(18,'Française','Acteur','1974-01-11');
+INSERT INTO CPOA_VIP VALUES(07,'Américaine','Acteur','1986-04-22');
+INSERT INTO CPOA_VIP VALUES(19,'Italienne','Acteur','1972-04-13');
+INSERT INTO CPOA_VIP VALUES(02,'Italienne','Réalisateur','1981-08-03');
+INSERT INTO CPOA_VIP VALUES(10,'Française','Réalisateur','1977-06-07');
+INSERT INTO CPOA_VIP VALUES(12,'Française','Réalisateur','1953-01-09');
+
+INSERT INTO CPOA_Celebrite VALUES (01,NULL,3,NULL);
+INSERT INTO CPOA_Celebrite VALUES (18,NULL,1,'Jean Dupont');
+INSERT INTO CPOA_Celebrite VALUES (19,NULL,1,NULL);
+INSERT INTO CPOA_Celebrite VALUES (12,NULL,2,'John Doe');
+
+INSERT INTO CPOA_Jure VALUES(02,1);
+INSERT INTO CPOA_Jure VALUES(03,1);
+INSERT INTO CPOA_Jure VALUES(06,2);
+INSERT INTO CPOA_Jure VALUES(08,3);
+INSERT INTO CPOA_Jure VALUES(11,4);
+INSERT INTO CPOA_Jure VALUES(13,4);
+INSERT INTO CPOA_Jure VALUES(16,5);
+INSERT INTO CPOA_Jure VALUES(17,5);
+
+INSERT INTO CPOA_Presse VALUES(4,1);
+INSERT INTO CPOA_Presse VALUES(5,3);
+INSERT INTO CPOA_Presse VALUES(7,3);
+INSERT INTO CPOA_Presse VALUES(9,2);
+INSERT INTO CPOA_Presse VALUES(10,1);
+INSERT INTO CPOA_Presse VALUES(14,1);
+INSERT INTO CPOA_Presse VALUES(15,2);
+INSERT INTO CPOA_Presse VALUES(20,3);
+
+INSERT INTO CPOA_Echange VALUES(01,04,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(02,08,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(03,07,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(04,18,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(05,19,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(06,10,'Fax',NULL);
+INSERT INTO CPOA_Echange VALUES(07,12,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(08,02,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(09,18,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(10,07,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(11,08,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(12,02,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(13,19,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(14,01,'Fax',NULL);
+INSERT INTO CPOA_Echange VALUES(15,04,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(16,01,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(17,19,'Tel',NULL);
+INSERT INTO CPOA_Echange VALUES(18,02,'Mail',NULL);
+INSERT INTO CPOA_Echange VALUES(19,18,'Fax',NULL);
+INSERT INTO CPOA_Echange VALUES(20,08,'Tel',NULL);
+
+
+
+
+INSERT INTO CPOA_Celebrite VALUES (numVIP,photo,coefImportance,Compagnon)
+--INSERT INTO CPOA_FilmProjection VALUES (numProjection, numFilm)
+INSERT INTO CPOA_Echange VALUES(numEchange,numVIP,modeCommunication,contenu)
+INSERT INTO CPOA_Jure VALUES(numJure,numConpetition)
+INSERT INTO CPOA_Presse VALUES(numPersonne,niveauAccreditation)
+INSERT INTO CPOA_Projection VALUES(numProjection,dateDebut,numSalle)
+INSERT INTO CPOA_VIP VALUES(numVIP,nationalite,metier,dateNaissance)
